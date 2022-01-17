3f010b4f94c80e365475cc24f385c1ae4cfe25a0
alinali87 <alinali87@gmail.com>
2022-01-17 09:52:49 +0300

    init commit

---
+++ hexlet_pytest/example.py
@@ -0,0 +1,3 @@
+def reverse(str):
+    return str[::-1]
+
---
+++ tests/test_example.py
@@ -0,0 +1,7 @@
+from hexlet_pytest.example import reverse
+
+def test_reverse():
+    assert reverse('Hexlet') == 'telxeH'
+
+def test_reverse_for_empty_string():
+    assert reverse('') == ''
